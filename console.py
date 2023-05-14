605097076f36550d71131b5d83b1760478dbba05
BibiObiora <bibiavril1998@gmail.com>
2023-05-14 09:22:40 -0700

    Update

--- console.py
+++ console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module for the entry point of the command interpreter."""
+"""Module for the entry point of the command interpreter. Edidiong and Joshua Ryan"""
 
 import cmd
 from models.base_model import BaseModel
